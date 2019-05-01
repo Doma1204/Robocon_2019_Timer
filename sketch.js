d5c1435e026b38262f37e629c448527874525503
Doma <josephlamyip@gmail.com>
2019-05-02 00:27:15 +0800

    formatting issue

--- sketch.js
+++ sketch.js
@@ -5,263 +5,263 @@ let isOneMinute = true;
 let a = [];
 
 function setup() {
-  timerText = document.getElementById("Timer");
-  startStopText = document.getElementById("StartBtn")
-  scoreText = document.getElementById("Score");
-  logText = document.getElementById("logText");
-  retryText = document.getElementById("retryBtn");
+	timerText = document.getElementById("Timer");
+	startStopText = document.getElementById("StartBtn")
+	scoreText = document.getElementById("Score");
+	logText = document.getElementById("logText");
+	retryText = document.getElementById("retryBtn");
 
-  startPauseBtn = document.getElementById("startPauseBtn");
-  stopClearBtn = document.getElementById("stopClearBtn");
+	startPauseBtn = document.getElementById("startPauseBtn");
+	stopClearBtn = document.getElementById("stopClearBtn");
 
-  MR1Btn1 = document.getElementById("MR1Btn1");
-  MR1Btn2 = document.getElementById("MR1Btn2");
-  MR1Btn3 = document.getElementById("MR1Btn3");
-  MR2Btn = document.getElementById("MR2Btn");
+	MR1Btn1 = document.getElementById("MR1Btn1");
+	MR1Btn2 = document.getElementById("MR1Btn2");
+	MR1Btn3 = document.getElementById("MR1Btn3");
+	MR2Btn = document.getElementById("MR2Btn");
 
-  $("input[name*='time-format']").click(updateTimeFormat);
-  $("input[name*='field']").click(updateFieldColor);
-  $("#prep-time").click(function(){
-    isOneMinute = this.checked;
-    timer.setTime(isOneMinute ? 1 : 3);
-    writeTimer(timer);
-  });
+	$("input[name*='time-format']").click(updateTimeFormat);
+	$("input[name*='field']").click(updateFieldColor);
+	$("#prep-time").click(function () {
+		isOneMinute = this.checked;
+		timer.setTime(isOneMinute ? 1 : 3);
+		writeTimer(timer);
+	});
 
-  timer = new Timer();
-  team = new Team();
-  timer.setTime(1);
-  timer.setTimerCallback(writeTimer);
-  timer.setTimerEndCallback(timerEnd);
+	timer = new Timer();
+	team = new Team();
+	timer.setTime(1);
+	timer.setTimerCallback(writeTimer);
+	timer.setTimerEndCallback(timerEnd);
 
-  reset();
+	reset();
 }
 
 function reset() {
-  // $("#start-btn").attr("disabled", "false");
-  document.getElementById("start-btn").disabled = false;
-  $(".timer-setting").removeClass("disappear");
-  $("#history").empty();
-  timer.resetTimer();
-  writeTimer(timer);
-  team.reset();
-  updateMR1Btn();
-  updateMR2Btn();
-  updateRetryBtn();
-  updateScore();
-  updateRetryNumber();
-  updateViolationNumber();
+	// $("#start-btn").attr("disabled", "false");
+	document.getElementById("start-btn").disabled = false;
+	$(".timer-setting").removeClass("disappear");
+	$("#history").empty();
+	timer.resetTimer();
+	writeTimer(timer);
+	team.reset();
+	updateMR1Btn();
+	updateMR2Btn();
+	updateRetryBtn();
+	updateScore();
+	updateRetryNumber();
+	updateViolationNumber();
 }
 
 function startPauseBtnOnClick() {
-  if (timer.state == TimerState.START) {
-    $("#start-btn").html("Start");
-    timer.pauseTimer();
-  } else {
-    $("#start-btn").html("Pause");
-    $("#reset-btn").html("Stop");
-    timer.startTimer();
-    $(".timer-setting").addClass("disappear");
-  }
+	if (timer.state == TimerState.START) {
+		$("#start-btn").html("Start");
+		timer.pauseTimer();
+	} else {
+		$("#start-btn").html("Pause");
+		$("#reset-btn").html("Stop");
+		timer.startTimer();
+		$(".timer-setting").addClass("disappear");
+	}
 }
 
 function stopClearBtnOnClick() {
-  if (timer.state == TimerState.STOP) {
-    reset();
-  } else {
-    $("#start-btn").attr("disabled", "true");
-    $("#start-btn").html("Start");
-    $("#reset-btn").html("Clear");
-    timer.stopTimer();
-  }
+	if (timer.state == TimerState.STOP) {
+		reset();
+	} else {
+		$("#start-btn").attr("disabled", "true");
+		$("#start-btn").html("Start");
+		$("#reset-btn").html("Clear");
+		timer.stopTimer();
+	}
 }
 
 function retryBtnOnClick() {
-  if (timer.state != TimerState.START || isOneMinute) return;
-  team.toggleRetry();
-  updateRetryBtn();
-  updateRetryNumber();
-  updateHistory();
+	if (timer.state != TimerState.START || isOneMinute) return;
+	team.toggleRetry();
+	updateRetryBtn();
+	updateRetryNumber();
+	updateHistory();
 }
 
 function violationBtnOnClick() {
-  if (timer.state != TimerState.START || isOneMinute) return;
-  team.violation();
-  updateRetryBtn();
-  updateRetryNumber();
-  updateViolationNumber();
-  updateHistory();
+	if (timer.state != TimerState.START || isOneMinute) return;
+	team.violation();
+	updateRetryBtn();
+	updateRetryNumber();
+	updateViolationNumber();
+	updateHistory();
 }
 
 function undoBtnOnClick() {
-  if (timer.state != TimerState.START || isOneMinute) return;
-  team.undo();
-  updateMR1Btn();
-  updateMR2Btn();
-  updateRetryBtn();
-  updateScore();
-  updateRetryNumber();
-  updateViolationNumber();
-  $("#history tr:last").remove();
-  $(".history").scrollTop(99999999);
+	if (timer.state != TimerState.START || isOneMinute) return;
+	team.undo();
+	updateMR1Btn();
+	updateMR2Btn();
+	updateRetryBtn();
+	updateScore();
+	updateRetryNumber();
+	updateViolationNumber();
+	$("#history tr:last").remove();
+	$(".history").scrollTop(99999999);
 }
 
 function MR1Btn1OnClick() {
-  if (timer.state != TimerState.START || isOneMinute) return;
-  if (!team.completeTaskMR1(Shagai)) return;
-  updateMR1Btn();
-  updateMR2Btn();
-  updateScore();
-  updateHistory();
+	if (timer.state != TimerState.START || isOneMinute) return;
+	if (!team.completeTaskMR1(Shagai)) return;
+	updateMR1Btn();
+	updateMR2Btn();
+	updateScore();
+	updateHistory();
 }
 
 function MR1Btn2OnClick() {
-  if (timer.state != TimerState.START || isOneMinute) return;
-  if (!team.completeTaskMR1(Camel)) return;
-  updateMR2Btn();
-  updateScore();
-  updateHistory();
+	if (timer.state != TimerState.START || isOneMinute) return;
+	if (!team.completeTaskMR1(Camel)) return;
+	updateMR2Btn();
+	updateScore();
+	updateHistory();
 }
 
 function MR1Btn3OnClick() {
-  if (timer.state != TimerState.START || isOneMinute) return;
-  if (!team.completeTaskMR1(Horse)) return;
-  updateMR2Btn();
-  updateScore();
-  updateHistory();
+	if (timer.state != TimerState.START || isOneMinute) return;
+	if (!team.completeTaskMR1(Horse)) return;
+	updateMR2Btn();
+	updateScore();
+	updateHistory();
 }
 
 function MR2BtnOnClick() {
-  if (timer.state != TimerState.START || isOneMinute) return;
-  if (!team.completeTaskMR2()) return;
-  updateMR1Btn();
-  updateMR2Btn();
-  updateScore();
-  updateHistory();
+	if (timer.state != TimerState.START || isOneMinute) return;
+	if (!team.completeTaskMR2()) return;
+	updateMR1Btn();
+	updateMR2Btn();
+	updateScore();
+	updateHistory();
 }
 
 function updateMR1Btn() {
-  if (team.curTaskMR1 == Shagai) {
-    $("#MR1Btn2").removeClass("hide");
-    $("#MR1Btn3").removeClass("hide");
-  } else {
-    $("#MR1Btn2").addClass("hide");
-    $("#MR1Btn3").addClass("hide");
-  }
+	if (team.curTaskMR1 == Shagai) {
+		$("#MR1Btn2").removeClass("hide");
+		$("#MR1Btn3").removeClass("hide");
+	} else {
+		$("#MR1Btn2").addClass("hide");
+		$("#MR1Btn3").addClass("hide");
+	}
 
-  $("#MR1Btn1").html(team.curTaskMR1);
+	$("#MR1Btn1").html(team.curTaskMR1);
 }
 
 function updateMR2Btn() {
-  $("#MR2Btn").html(team.curTaskMR2);
+	$("#MR2Btn").html(team.curTaskMR2);
 }
 
 function updateScore() {
-  $("#Score").html(team.score);
+	$("#Score").html(team.score);
 }
 
 function updateRetryBtn() {
-  $("#retry-btn").html(team.onRetry ? "End Retry" : "Start Retry");
+	$("#retry-btn").html(team.onRetry ? "End Retry" : "Start Retry");
 }
 
 function updateRetryNumber() {
-  $("#retry-time").html(team.numOfRetry);
+	$("#retry-time").html(team.numOfRetry);
 }
 function updateViolationNumber() {
-  $("#violation-time").html(team.numOfViolation);
+	$("#violation-time").html(team.numOfViolation);
 }
 
 function updateHistory(oldEvent = undefined) {
-  let event = oldEvent == undefined ? team.event[team.event.length - 1] : oldEvent;
-  let eventString;
-  if (event.event == "Retry Start") {
-    if (team.event.length >= 2 && team.event[team.event.length - 2].event == "Violation") {
-      updateHistory(team.event[team.event.length - 2]);
-    }
-    eventString = "Retry Start";
-  } else if (event.event == "Retry End") {
-    let lastEvents = team.findAllEvent("Retry Start");
-    let startTime = lastEvents[lastEvents.length - 1].time;
-    let time = Math.round((event.time - startTime) / 100) / 10;
-    eventString = event.event + " (Time taken: " + time + "s)";
-  } else if (event.event.includes("Line")) {
-    eventString = "Pass " + event.event;
-  } else if (event.event == Shagai) {
-    eventString = "Shagai Land";
-  } else if (event.event == Camel || event.event == Horse) {
-    eventString = "Shagai Land as " + event.event;
-  } else {
-    eventString = event.event;
-  }
-  eventString += " (Score: " + team.score + ")";
-  $("#history").append("<tr><th>" + event.timeString + "</th><th>" + eventString);
-  $(".history").scrollTop(99999999);
+	let event = oldEvent == undefined ? team.event[team.event.length - 1] : oldEvent;
+	let eventString;
+	if (event.event == "Retry Start") {
+		if (team.event.length >= 2 && team.event[team.event.length - 2].event == "Violation") {
+			updateHistory(team.event[team.event.length - 2]);
+		}
+		eventString = "Retry Start";
+	} else if (event.event == "Retry End") {
+		let lastEvents = team.findAllEvent("Retry Start");
+		let startTime = lastEvents[lastEvents.length - 1].time;
+		let time = Math.round((event.time - startTime) / 100) / 10;
+		eventString = event.event + " (Time taken: " + time + "s)";
+	} else if (event.event.includes("Line")) {
+		eventString = "Pass " + event.event;
+	} else if (event.event == Shagai) {
+		eventString = "Shagai Land";
+	} else if (event.event == Camel || event.event == Horse) {
+		eventString = "Shagai Land as " + event.event;
+	} else {
+		eventString = event.event;
+	}
+	eventString += " (Score: " + team.score + ")";
+	$("#history").append("<tr><th>" + event.timeString + "</th><th>" + eventString);
+	$(".history").scrollTop(99999999);
 }
 
 function updateTeamName(teamName) {
-  $(".team-name").html(teamName);
+	$(".team-name").html(teamName);
 }
 
 function updateFieldColor() {
-  if ($("input[name='field']:checked").val() == "red") {
-    $(".blue-field").addClass("red-field");
-    $(".blue-field").removeClass("blue-field");
-  } else {
-    $(".red-field").addClass("blue-field");
-    $(".red-field").removeClass("red-field");
-  }
+	if ($("input[name='field']:checked").val() == "red") {
+		$(".blue-field").addClass("red-field");
+		$(".blue-field").removeClass("blue-field");
+	} else {
+		$(".red-field").addClass("blue-field");
+		$(".red-field").removeClass("red-field");
+	}
 }
 
 function updateTimeFormat() {
-  switch ($("input[name='time-format']:checked").val()) {
-    case "MSm":
-      timer.setStyle(TimerStyle.MSm);
-      $("#timer-minute").removeClass("hide");
-      $("#timer-millis").removeClass("hide");
-      break;
+	switch ($("input[name='time-format']:checked").val()) {
+		case "MSm":
+			timer.setStyle(TimerStyle.MSm);
+			$("#timer-minute").removeClass("hide");
+			$("#timer-millis").removeClass("hide");
+			break;
 
-    case "MS":
-      timer.setStyle(TimerStyle.MS);
-      $("#timer-minute").removeClass("hide");
-      $("#timer-millis").addClass("hide");
-      break;
+		case "MS":
+			timer.setStyle(TimerStyle.MS);
+			$("#timer-minute").removeClass("hide");
+			$("#timer-millis").addClass("hide");
+			break;
 
-    case "S":
-      timer.setStyle(TimerStyle.S);
-      $("#timer-minute").addClass("hide");
-      $("#timer-millis").addClass("hide");
-      break;
-  }
-  writeTimer(timer);
+		case "S":
+			timer.setStyle(TimerStyle.S);
+			$("#timer-minute").addClass("hide");
+			$("#timer-millis").addClass("hide");
+			break;
+	}
+	writeTimer(timer);
 }
 
 function writeTimer(timer) {
-  let time = timer.getTime();
-  switch (timer.style) {
-    case TimerStyle.MSm:
-    case TimerStyle.MS:
-      $("#timer-minute").html(fillZero(time.minute, 2) + ":");
-      $("#timer-second").html(fillZero(time.second, 2));
-      $("#timer-millis").html("." + fillZero(time.millisecond, 3));
-      break;
+	let time = timer.getTime();
+	switch (timer.style) {
+		case TimerStyle.MSm:
+		case TimerStyle.MS:
+			$("#timer-minute").html(fillZero(time.minute, 2) + ":");
+			$("#timer-second").html(fillZero(time.second, 2));
+			$("#timer-millis").html("." + fillZero(time.millisecond, 3));
+			break;
 
-    case TimerStyle.S:
-      $("#timer-second").html(fillZero(time.minute * 60 + time.second, 3));
-      break;
-  }
+		case TimerStyle.S:
+			$("#timer-second").html(fillZero(time.minute * 60 + time.second, 3));
+			break;
+	}
 }
 
 function timerEnd() {
-  if (isOneMinute) {
-    isOneMinute = false;
-    timer.setTime(3);
-    timer.startTimer();
-    return;
-  } else {
-    timer.setTime($("#prep-time").prop("checked") ? 1 : 3);
-  }
-  $("#start-btn").attr("disabled", "true");
-  $("#start-btn").html("Start");
-  $("#reset-btn").html("Clear");
+	if (isOneMinute) {
+		isOneMinute = false;
+		timer.setTime(3);
+		timer.startTimer();
+		return;
+	} else {
+		timer.setTime($("#prep-time").prop("checked") ? 1 : 3);
+	}
+	$("#start-btn").attr("disabled", "true");
+	$("#start-btn").html("Start");
+	$("#reset-btn").html("Clear");
 }
 
 // function keyPressed(event) {
--- timer.js
+++ timer.js
@@ -3,127 +3,127 @@ const TimerMode = Object.freeze({ CountUp: Symbol("CountUp"), CountDown: Symbol(
 const TimerStyle = Object.freeze({ MSm: Symbol("MSm"), MS: Symbol("MS"), S: Symbol("S") });  // MSm: "00:00:000", MS: "00;00", S: "000"
 
 class Timer {
-  constructor(minute = NaN, second = NaN, mode = TimerMode.CountDown, style = TimerStyle.MSm) {
-    this.state = TimerState.STOP;
-    this.setMode(mode);
-    this.setStyle(style);
-    this.currentTime = 0;
-    this.lastTime = 0;
-    this.setTime(minute, second);
-    this.timerCallback = undefined;
-    this.timerEndCallback = undefined;
-    this.interval = undefined;
-  }
+	constructor(minute = NaN, second = NaN, mode = TimerMode.CountDown, style = TimerStyle.MSm) {
+		this.state = TimerState.STOP;
+		this.setMode(mode);
+		this.setStyle(style);
+		this.currentTime = 0;
+		this.lastTime = 0;
+		this.setTime(minute, second);
+		this.timerCallback = undefined;
+		this.timerEndCallback = undefined;
+		this.interval = undefined;
+	}
 
-  setTime(minute = NaN, second = NaN) {
-    this.endTime = 0;
+	setTime(minute = NaN, second = NaN) {
+		this.endTime = 0;
 
-    if (minute)
-      this.endTime += minute * 60000;
-    if (second)
-      this.endTime += second * 1000;
-  }
+		if (minute)
+			this.endTime += minute * 60000;
+		if (second)
+			this.endTime += second * 1000;
+	}
 
-  setMode(mode) {
-    this.mode = mode;
-  }
+	setMode(mode) {
+		this.mode = mode;
+	}
 
-  setStyle(style) {
-    this.style = style;
-  }
+	setStyle(style) {
+		this.style = style;
+	}
 
-  setTimerCallback(callback) {
-    this.timerCallback = callback;
-  }
+	setTimerCallback(callback) {
+		this.timerCallback = callback;
+	}
 
-  setTimerEndCallback(callback) {
-    this.timerEndCallback = callback;
-  }
+	setTimerEndCallback(callback) {
+		this.timerEndCallback = callback;
+	}
 
-  startTimer() {
-    if (timer.state == TimerState.STOP && timer.endTime) {
-      console.log("Start Timer");
-      this.currentTime = 0;
-      this.lastTime = new Date().getTime();
-      this.state = TimerState.START;
-      this.interval = setInterval(() => { this.countTimer() }, 1);
-    } else if (this.state == TimerState.PAUSE) {
-      this.state = TimerState.START;
-      this.lastTime = new Date().getTime();
-      this.interval = setInterval(() => { this.countTimer() }, 1);
-    }
-  }
+	startTimer() {
+		if (timer.state == TimerState.STOP && timer.endTime) {
+			console.log("Start Timer");
+			this.currentTime = 0;
+			this.lastTime = new Date().getTime();
+			this.state = TimerState.START;
+			this.interval = setInterval(() => { this.countTimer() }, 1);
+		} else if (this.state == TimerState.PAUSE) {
+			this.state = TimerState.START;
+			this.lastTime = new Date().getTime();
+			this.interval = setInterval(() => { this.countTimer() }, 1);
+		}
+	}
 
-  pauseTimer() {
-    if (this.state == TimerState.START) {
-      this.state = TimerState.PAUSE;
-      clearInterval(this.interval);
-    }
-  }
+	pauseTimer() {
+		if (this.state == TimerState.START) {
+			this.state = TimerState.PAUSE;
+			clearInterval(this.interval);
+		}
+	}
 
-  stopTimer() {
-    this.state = TimerState.STOP;
-    clearInterval(this.interval);
-  }
+	stopTimer() {
+		this.state = TimerState.STOP;
+		clearInterval(this.interval);
+	}
 
-  resetTimer() {
-    this.state = TimerState.STOP;
-    this.currentTime = 0;
-    clearInterval(this.interval);
-  }
+	resetTimer() {
+		this.state = TimerState.STOP;
+		this.currentTime = 0;
+		clearInterval(this.interval);
+	}
 
-  countTimer() {
-    let currentTime = new Date().getTime();
-    this.currentTime += currentTime - this.lastTime;
-    this.lastTime = currentTime;
+	countTimer() {
+		let currentTime = new Date().getTime();
+		this.currentTime += currentTime - this.lastTime;
+		this.lastTime = currentTime;
 
-    if (this.currentTime >= this.endTime) {
-      this.currentTime = this.endTime;
-      if (this.timerCallback)
-        this.timerCallback(this);
+		if (this.currentTime >= this.endTime) {
+			this.currentTime = this.endTime;
+			if (this.timerCallback)
+				this.timerCallback(this);
 
-      this.stopTimer();
-      if (this.timerEndCallback)
-        this.timerEndCallback(this);
-      return;
-    }
+			this.stopTimer();
+			if (this.timerEndCallback)
+				this.timerEndCallback(this);
+			return;
+		}
 
-    if (this.timerCallback)
-      this.timerCallback(this);
-  }
+		if (this.timerCallback)
+			this.timerCallback(this);
+	}
 
-  getRawTime() {
-    return this.currentTime;
-  }
+	getRawTime() {
+		return this.currentTime;
+	}
 
-  getTime(mode = undefined) {
-    if (!mode)
-      mode = this.mode;
+	getTime(mode = undefined) {
+		if (!mode)
+			mode = this.mode;
 
-    let timeCalculate = mode == TimerMode.CountUp ? this.currentTime : this.endTime - this.currentTime;
-    let time = { minute: 0, second: 0, millisecond: 0 };
-    time.millisecond = timeCalculate % 1000;
-    time.second = Math.floor(timeCalculate / 1000);
-    time.minute = Math.floor(time.second / 60);
-    time.second -= time.minute * 60;
+		let timeCalculate = mode == TimerMode.CountUp ? this.currentTime : this.endTime - this.currentTime;
+		let time = { minute: 0, second: 0, millisecond: 0 };
+		time.millisecond = timeCalculate % 1000;
+		time.second = Math.floor(timeCalculate / 1000);
+		time.minute = Math.floor(time.second / 60);
+		time.second -= time.minute * 60;
 
-    return time;
-  }
+		return time;
+	}
 
-  getTimeString(mode = undefined, style = undefined) {
-    let time = this.getTime(mode);
-    if (!style)
-      style = this.style;
+	getTimeString(mode = undefined, style = undefined) {
+		let time = this.getTime(mode);
+		if (!style)
+			style = this.style;
 
-    if (style == TimerStyle.MSm)
-      return fillZero(time.minute, 2) + ":" + fillZero(time.second, 2) + "." + fillZero(time.millisecond, 3);
-    else if (style == TimerStyle.MS)
-      return fillZero(time.minute, 2) + ":" + fillZero(time.second, 2);
-    else if (style == TimerStyle.S)
-      return (time.minute * 60 + time.second).toString();
-  }
+		if (style == TimerStyle.MSm)
+			return fillZero(time.minute, 2) + ":" + fillZero(time.second, 2) + "." + fillZero(time.millisecond, 3);
+		else if (style == TimerStyle.MS)
+			return fillZero(time.minute, 2) + ":" + fillZero(time.second, 2);
+		else if (style == TimerStyle.S)
+			return (time.minute * 60 + time.second).toString();
+	}
 }
 
 function fillZero(number, padding) {
-  return ("0".repeat(padding) + number).slice(-padding);
+	return ("0".repeat(padding) + number).slice(-padding);
 }
